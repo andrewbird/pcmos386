617519b1a9ecdac1c860d197edf82143402f49f3
Andrew Bird <ajb@spheresystems.co.uk>
2019-08-08 23:43:49 +0100

    Travis: Use FreeDOS 1.20

--- travis_build.sh
+++ travis_build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-cd SOURCES/src || exit 1
-
 mkdir -p ${HOME}/.dosemu/run
 touch ${HOME}/.dosemu/disclaimer
+
+[ -d ${HOME}/.dosemu/drive_c ] || mkdir ${HOME}/.dosemu/drive_c
+tar -C ${HOME}/.dosemu/drive_c -xvf FR-DOS-1.20.tar kernel.sys command.com
+cp /usr/share/dosemu/commands/fdconfig.sys ${HOME}/.dosemu/drive_c/.
+cp /usr/share/dosemu/commands/autoexec.bat ${HOME}/.dosemu/drive_c/.
+
+cd SOURCES/src || exit 1
 
 ./build.sh
